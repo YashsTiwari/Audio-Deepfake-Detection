0504adeba5f6fbacf12d8719e3e0b98da93e53a0
Yashs Tiwari <yashstiwari03@gmail.com>
2026-04-19 03:13:05 +0530

    Remove unnecessary audio and log files

--- data.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="XuepingZhang/ESDD2-CompSpoof-V2",
-    repo_type="dataset",
-    local_dir="./CompSpoofV2",
-    max_workers=8,
-)
